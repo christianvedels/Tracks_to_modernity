f7f0cdbdaad1607c8c63cd97d692999a480c89ae
TomGoerges <144690618+TomGoerges@users.noreply.github.com>
2025-02-04 11:33:57 +0100

    Regressions
    Rerun Regressions

--- 201_Regressions_without_nodes.R
+++ 201_Regressions_without_nodes.R
@@ -2,12 +2,13 @@
 #
 # Date updated:   2025-01-21
 # Author:         Christian Vedel, Tom Görges
-# Purpose:        Runs regressions
+# Purpose:        Runs regressions after dropping nodes
 
 # ==== Libraries ====
 library(tidyverse)
 library(fixest)
 library(did)
+library(readxl)
 library(psych) # for summary stats
 library(kableExtra) # for latex tables
 source("Data_cleaning_scripts/000_Functions.R")
@@ -112,313 +113,23 @@ grundtvig <- grundtvig %>%
 # Calculate and display the number of observations removed
 cat("Number of observations removed due to duplicate removal:", original_rows - nrow(grundtvig), "\n")
 
-# === Summary Statistics ===
+############################
+# === Filter out nodes === #
+############################
 
-# 1) Census data
-stats_census <- describe(census[, c("Population", 
-                           "Share_Manufacturing", 
-                           "Child_women_ratio", 
-                           "HISCAM", 
-                           "Migration", 
-                           "RailAccess",
-                           "LCPAccess")])
+# load nodes_distance data
+distance_to_nodes <- read_excel("Data/distance_to_nodes.xlsx")
 
+distance_to_nodes <- distance_to_nodes %>%
+  mutate(GIS_ID = as.character(GIS_ID))
 
-# Select only relevant columns
-stats_selected <- stats_census[, c("n", "mean", "sd", "min", "max")]
-
-stats_selected <- stats_selected %>%
-  mutate(across(c(mean, sd, min, max), ~ round(.x, 3)))
+census <- left_join(census, distance_to_nodes, by = "GIS_ID")
+grundtvig <- left_join(grundtvig, distance_to_nodes, by = "GIS_ID")
 
 
-# Create summary statistics latex table
-kbl(stats_selected, 
-    booktabs = T, 
-    caption = "Summary Statistics: Railways and Local Development", 
-    row.names = T,
-    align = "ccccc",
-    linesep = "",  # Suppress additional line spacing
-    format = "latex") %>%
-  kable_styling(
-    latex_options = c("hold_position", "scale_down") # Ensures [!h] placement and that it firs the page
-  ) %>%
-  group_rows("Panel A: Economy", 1, 5) %>%
-  group_rows("Panel B: Infrastructure", 6, 7) %>%
-  footnote(general = "Here is a general comments of the table. ")
-
-
-# 2) Grundtvig data
-stats_grundtvig <- describe(grundtvig[, c("Assembly_house", 
-                                    "HighSchool",
-                                    "RailAccess",
-                                    "LCPAccess")])
-
-
-# Select only relevant columns
-stats_selected <- stats_grundtvig[, c("n", "mean", "sd", "min", "max")]
-
-stats_selected <- stats_selected %>%
-  mutate(across(c(mean, sd, min, max), ~ round(.x, 3)))
-
-
-# Create summary statistics latex table
-kbl(stats_selected, 
-    booktabs = T, 
-    caption = "Summary Statistics: Railways and Grundtvigianism", 
-    row.names = T,
-    align = "ccccc",
-    linesep = "",  # Suppress additional line spacing
-    format = "latex") %>%
-  kable_styling(
-    latex_options = c("hold_position", "scale_down") # Ensures [!h] placement
-  ) %>%
-  group_rows("Panel A: Grundtvig", 1, 2) %>%
-  group_rows("Panel B: Infrastructure", 3, 4)  %>%
-  footnote(general = "Here is a general comments of the table. ")
-
-
-# ==== TWFE regressions (Census data) ====
-twfe1 = feols(
-  lnPopulation ~ RailAccess | GIS_ID + Year,
-  data = census,
-  cluster = ~ GIS_ID
-)
-
-twfe2 = feols(
-  lnChild_women_ratio ~ RailAccess | GIS_ID + Year,
-  data = census,
-  cluster = ~ GIS_ID
-)
-
-twfe3 = feols(
-  Share_Manufacturing ~ RailAccess | GIS_ID + Year,
-  data = census,
-  cluster = ~ GIS_ID
-)
-
-twfe4 = feols(
-  HISCAM ~ RailAccess | GIS_ID + Year,
-  data = census,
-  cluster = ~ GIS_ID
-)
-
-twfe5 = feols(
-  lnMigration ~ RailAccess | GIS_ID + Year,
-  data = census,
-  cluster = ~ GIS_ID
-)
-
-# View results
-etable(twfe1, twfe2, twfe3, twfe4, twfe5,
-       fitstat = ~ ar2 + n,  # Include R-squared and number of observations
-       cluster = "GIS_ID", # Display the clustering
-       signif.code = c("***" = 0.01, "**" = 0.05, "*" = 0.1)) # Custom significance codes
-
-
-# ==== Doubly Robust DID: Development ====
-
-# log(Pop)
-cs_mod1 <- att_gt(
-  yname = "lnPopulation",          # Outcome variable
-  tname = "Year_num",             # Time variable
-  idname = "GIS_ID_num",          # Unit identifier
-  gname = "Treat_year",       # First year of treatment
-  xformla = ~1,               # No covariates 
-  data = census,              # Your dataset
-  clustervars = "GIS_ID"      # Cluster variable
-)
-
-# Child-woman ratio
-cs_mod2 <- att_gt(
-  yname = "lnChild_women_ratio",          # Outcome variable
-  tname = "Year_num",             # Time variable
-  idname = "GIS_ID_num",          # Unit identifier
-  gname = "Treat_year",       # First year of treatment
-  xformla = ~1,               # No covariates (consistent with TWFE)
-  data = census,              # Your dataset
-  clustervars = "GIS_ID"      # Cluster variable
-)
-
-# log Manufacturing
-cs_mod3 <- att_gt(
-  yname = "Share_Manufacturing",          # Outcome variable
-  tname = "Year_num",             # Time variable
-  idname = "GIS_ID_num",          # Unit identifier
-  gname = "Treat_year",       # First year of treatment
-  xformla = ~1,               # No covariates (consistent with TWFE)
-  data = census,              # Your dataset
-  clustervars = "GIS_ID"      # Cluster variable
-)
-
-# HISCAM
-cs_mod4 <- att_gt(
-  yname = "HISCAM",          # Outcome variable
-  tname = "Year_num",             # Time variable
-  idname = "GIS_ID_num",          # Unit identifier
-  gname = "Treat_year",       # First year of treatment
-  xformla = ~1,               # No covariates (consistent with TWFE)
-  data = census,              # Your dataset
-  clustervars = "GIS_ID"      # Cluster variable
-)
-
-# Migration
-cs_mod5 <- att_gt(
-  yname = "lnMigration",          # Outcome variable
-  tname = "Year_num",             # Time variable
-  idname = "GIS_ID_num",          # Unit identifier
-  gname = "Treat_year",       # First (observed) year of treatment
-  xformla = ~1,               # No covariates (consistent with TWFE)
-  data = census,              # Your dataset
-  clustervars = "GIS_ID"      # Cluster variable
-)
-
-# Aggregate the ATT
-agg_mod1 <- aggte(cs_mod1, type = "simple")
-agg_mod2 <- aggte(cs_mod2, type = "simple")
-agg_mod3 <- aggte(cs_mod3, type = "simple")
-agg_mod4 <- aggte(cs_mod4, type = "simple")
-agg_mod5 <- aggte(cs_mod5, type = "simple")
-
-# Summary
-summary(agg_mod1)
-summary(agg_mod2)
-summary(agg_mod3)
-summary(agg_mod4)
-summary(agg_mod5)
-
-
-# === Create Latex output table ===
-
-# Store p-values for significance ***
-
-# Define models for each set
-twfe_models <- list(twfe1, twfe2, twfe3, twfe4, twfe5)
-did_models <- list(agg_mod1, agg_mod2, agg_mod3, agg_mod4, agg_mod5)
-
-# Compute p-values for TWFE models
-twfe_p <- unlist(lapply(twfe_models, function(model) {
-  pnorm(abs(model$coefficients / model$se), lower.tail = FALSE) * 2
-}))
-
-# Compute p-values for DID models
-did_p <- unlist(lapply(did_models, function(model) {
-  pnorm(abs(model$overall.att / model$overall.se), lower.tail = FALSE) * 2
-}))
-
-
-# Dynamically create the Outcomes header mapping
-outcomes_twfe <- c(as.character(formula(twfe1)[2]), 
-            as.character(formula(twfe2)[2]), 
-            as.character(formula(twfe3)[2]), 
-            as.character(formula(twfe4)[2]), 
-            as.character(formula(twfe5)[2]))
-
-outcomes_cs <- c(agg_mod1$DIDparams$yname,
-                        agg_mod2$DIDparams$yname,
-                        agg_mod3$DIDparams$yname,
-                        agg_mod4$DIDparams$yname,
-                        agg_mod5$DIDparams$yname)
-
-
-# Check if headers align
-if (!identical(outcomes_twfe, outcomes_cs)) {
-  stop("Models don't align!")
-}
-
-# Create the named vector for add_header_above if they align
-outcomes_header <- c(" " = 1, setNames(rep(1, length(outcomes_twfe)), outcomes_twfe))
-
-
-twfe_coef <- c("RailAccess", sprintf("%.3f", twfe1$coefficients), sprintf("%.3f", twfe2$coefficients), sprintf("%.3f", twfe3$coefficients), sprintf("%.3f", twfe4$coefficients), sprintf("%.3f", twfe5$coefficients))
-
-# Add ***
-twfe_coef[2:6] <- sapply(2:6, function(i) {
-  coef_value <- as.numeric(twfe_coef[i])
-  p_value <- as.numeric(twfe_p[i-1])
-  if (!is.na(p_value)) {
-    if (p_value < 0.01) {
-      sprintf("%.3f***", coef_value)
-    } else if (p_value < 0.05) {
-      sprintf("%.3f**", coef_value)
-    } else if (p_value < 0.1) {
-      sprintf("%.3f*", coef_value)
-    } else {
-      sprintf("%.3f", coef_value)
-    }
-  } else {
-    sprintf("%.3f", coef_value)
-  }
-})
-
-# built output table row by row
-twfe_se <- c("", sprintf("(%.3f)", twfe1$se), sprintf("(%.3f)",twfe2$se), sprintf("(%.3f)",twfe3$se), sprintf("(%.3f)",twfe4$se), sprintf("(%.3f)",twfe5$se))
-parish_fe <- c("Parish FE", "Yes", "Yes", "Yes", "Yes", "Yes")
-year_fe <- c("Year FE", "Yes", "Yes", "Yes", "Yes", "Yes")
-twfe_obs <- c("Obs.", twfe1$nobs, twfe2$nobs, twfe3$nobs, twfe4$nobs, twfe5$nobs)
-did_coef <- c("RailAccess", sprintf("%.3f",agg_mod1$overall.att), sprintf("%.3f",agg_mod2$overall.att), sprintf("%.3f",agg_mod3$overall.att), sprintf("%.3f", agg_mod4$overall.att), sprintf("%.3f",agg_mod5$overall.att))
-
-# Add significance stars to DID coefficients based on p-values
-did_coef[2:6] <- sapply(2:6, function(i) {
-  coef_value <- as.numeric(did_coef[i])
-  p_value <- as.numeric(did_p[i-1])
-  if (!is.na(p_value)) {
-    if (p_value < 0.01) {
-      sprintf("%.3f***", coef_value)
-    } else if (p_value < 0.05) {
-      sprintf("%.3f**", coef_value)
-    } else if (p_value < 0.1) {
-      sprintf("%.3f*", coef_value)
-    } else {
-      sprintf("%.3f", coef_value)
-    }
-  } else {
-    sprintf("%.3f", coef_value)
-  }
-})
-
-
-did_se <- c("", sprintf("(%.3f)", agg_mod1$overall.se), sprintf("(%.3f)", agg_mod2$overall.se), sprintf("(%.3f)", agg_mod3$overall.se), sprintf("(%.3f)", agg_mod4$overall.se), sprintf("(%.3f)", agg_mod5$overall.se))
-
-did_obs <- c("Obs.", agg_mod1$DIDparams$n * agg_mod1$DIDparams$nT, agg_mod2$DIDparams$n* agg_mod2$DIDparams$nT, agg_mod3$DIDparams$n* agg_mod3$DIDparams$nT, agg_mod4$DIDparams$n* agg_mod4$DIDparams$nT, agg_mod5$DIDparams$n* agg_mod5$DIDparams$nT)
-
-# Create mean Outcome
-
-# Dynamically calculate the means for the specified columns
-mean_outcome <- c(
-  "Mean of Outcome",
-  sapply(outcomes_cs, function(col) sprintf("%.3f", mean(census[[col]], na.rm = TRUE)))
-)
-
-did_n_parishes <- c("Parishes", length(unique(agg_mod1$DIDparams$data$GIS_ID)), length(unique(agg_mod2$DIDparams$data$GIS_ID)), length(unique(agg_mod3$DIDparams$data$GIS_ID)), length(unique(agg_mod4$DIDparams$data$GIS_ID)), length(unique(agg_mod5$DIDparams$data$GIS_ID)))
-
-
-# bind together for output table
-results <- rbind(twfe_coef, twfe_se, parish_fe, year_fe, twfe_obs, did_coef, did_se, did_obs, did_n_parishes, mean_outcome)
-colnames(results) <- c(" ", "(1)", "(2)", "(3)", "(4)", "(5)")
-
-
-# Create Output latex table
-kbl(results, 
-    booktabs = T, 
-    caption = "Railways and Local Development", 
-    row.names = F,
-    align = "lccccc",
-    linesep = "",  # Suppress additional line spacing
-    format = "latex") %>%
-  kable_styling(
-    latex_options = c("hold_position", "scale_down") # Ensures [!h] placement
-  ) %>%
-  add_header_above(outcomes_header) %>%
-  add_header_above(c(" " = 1, "Dependent variable:" = 5), escape = F) %>%
-  group_rows("Panel A: TWFE", 1, 5) %>%
-  group_rows("Panel B: Callaway and Sant'Anna", 6, 9) %>%
-  footnote(
-    general = "Notes: This table...",
-    number = c("Clustered (Parish) Standard errors in parentheses."),
-    symbol = c("Significance levels: * p<0.10, ** p<0.05, *** p<0.01"),
-    threeparttable = TRUE, # Ensures LaTeX handles the footnotes correctly
-    escape = FALSE          # Allows LaTeX symbols in the notes
-  )
+# filter 10 km
+census <- census %>% filter(min_distance_to_node_km > 10)
+grundtvig <- grundtvig %>% filter(min_distance_to_node_km > 10)
 
 
 ##########################################
@@ -471,8 +182,10 @@ tsls5 = feols(
 etable(tsls1$iv_first_stage[[1]], tsls2$iv_first_stage[[1]], tsls3$iv_first_stage[[1]],
        tsls4$iv_first_stage[[1]], tsls5$iv_first_stage[[1]], 
        tex = TRUE,
-       title = "First-Stage Estimates (With Nodes)")
+       title = "First-Stage Estimates (Nodes dropped)")
 
+
+#-----------------------------------------------------------------
 # Reduced form TWFE with Instrument
 twfe1_red = feols(
   lnPopulation ~ LCPAccess | GIS_ID + Year,
@@ -618,10 +331,10 @@ outcomes_twfe_reduced <- c(
 )
 
 outcomes_cs_reduced <- c(agg_mod1$DIDparams$yname,
-                 agg_mod2$DIDparams$yname,
-                 agg_mod3$DIDparams$yname,
-                 agg_mod4$DIDparams$yname,
-                 agg_mod5$DIDparams$yname)
+                         agg_mod2$DIDparams$yname,
+                         agg_mod3$DIDparams$yname,
+                         agg_mod4$DIDparams$yname,
+                         agg_mod5$DIDparams$yname)
 
 
 # Check if headers align
@@ -725,8 +438,6 @@ mean_outcome <- c(
 
 n_parishes <- c("Parishes", length(unique(agg_mod1$DIDparams$data$GIS_ID)), length(unique(agg_mod2$DIDparams$data$GIS_ID)), length(unique(agg_mod3$DIDparams$data$GIS_ID)), length(unique(agg_mod4$DIDparams$data$GIS_ID)), length(unique(agg_mod5$DIDparams$data$GIS_ID)))
 
-nodes_dropped <- c("Nodes dropped (<10km)", "No", "No", "No", "No", "No")
-  
 # first stage f-statistic
 firstF <- c("F-test (1st stage)", 
             round(as.numeric(fitstat(tsls1, "ivf")$ivf[1]), 2),
@@ -734,8 +445,9 @@ firstF <- c("F-test (1st stage)",
             round(as.numeric(fitstat(tsls3, "ivf")$ivf[1]), 2),
             round(as.numeric(fitstat(tsls4, "ivf")$ivf[1]), 2),
             round(as.numeric(fitstat(tsls5, "ivf")$ivf[1]), 2)
-            )
+)
 
+nodes_dropped <- c("Nodes dropped (<10km)", "Yes", "Yes", "Yes", "Yes", "Yes")
 
 
 # bind together for output table
@@ -773,185 +485,6 @@ kbl(results,
 # ==== Grundtvig === #
 ######################
 
-# TWFE
-mod1 = feols(
-  Assembly_house ~ Connected_rail | GIS_ID + Year,
-  data = grundtvig,
-  cluster = ~ GIS_ID
-)
-
-mod2 = feols(
-  HighSchool ~ Connected_rail | GIS_ID + Year,
-  data = grundtvig,
-  cluster = ~ GIS_ID
-)
-
-# View results
-etable(mod1, mod2)
-
-
-
-# ==== Doubly Robust DID: Grundtvig ====
-
-# No covariates
-out1 = att_gt(
-  yname = "Assembly_house",
-  tname = "Year_num",
-  gname = "Treat_year",
-  idname = "GIS_ID_num",
-  data = grundtvig,
-)
-
-out2 = att_gt(
-  yname = "HighSchool",
-  tname = "Year_num",
-  gname = "Treat_year",
-  idname = "GIS_ID_num",
-  data = grundtvig,
-)
-
-# Aggregate the ATT
-agg_mod1 <- aggte(out1, type = "simple", na.rm = T)
-agg_mod2 <- aggte(out2, type = "simple", na.rm = T)
-
-# Summary
-summary(agg_mod1)
-summary(agg_mod2)
-
-
-# === Create Latex output table ===
-
-# Store p-values for significance ***
-
-# Define models for each set
-twfe_models <- list(mod1, mod2)
-did_models <- list(agg_mod1, agg_mod2)
-
-# Compute p-values for TWFE models
-twfe_p <- unlist(lapply(twfe_models, function(model) {
-  pnorm(abs(model$coefficients / model$se), lower.tail = FALSE) * 2
-}))
-
-# Compute p-values for DID models
-did_p <- unlist(lapply(did_models, function(model) {
-  pnorm(abs(model$overall.att / model$overall.se), lower.tail = FALSE) * 2
-}))
-
-
-# Dynamically create the Outcomes header mapping
-outcomes_twfe <- c(as.character(formula(mod1)[2]), 
-                   as.character(formula(mod2)[2]))
-
-outcomes_cs <- c(agg_mod1$DIDparams$yname,
-                 agg_mod2$DIDparams$yname)
-
-
-# Check if headers align
-if (!identical(outcomes_twfe, outcomes_cs)) {
-  stop("Models don't align!")
-}
-
-# Create the named vector for add_header_above if they align
-outcomes_header <- c(" " = 1, setNames(rep(1, length(outcomes_twfe)), outcomes_twfe))
-
-# Built output table row by row
-
-# TWFE Coefficients
-twfe_coef <- c("RailAccess", sprintf("%.3f", mod1$coefficients), sprintf("%.3f", mod2$coefficients))
-
-# Add ***
-twfe_coef[2:3] <- sapply(2:3, function(i) {
-  coef_value <- as.numeric(twfe_coef[i])
-  p_value <- as.numeric(twfe_p[i-1])
-  if (!is.na(p_value)) {
-    if (p_value < 0.01) {
-      sprintf("%.3f***", coef_value)
-    } else if (p_value < 0.05) {
-      sprintf("%.3f**", coef_value)
-    } else if (p_value < 0.1) {
-      sprintf("%.3f*", coef_value)
-    } else {
-      sprintf("%.3f", coef_value)
-    }
-  } else {
-    sprintf("%.3f", coef_value)
-  }
-})
-
-# TWFE SEs
-twfe_se <- c("", sprintf("(%.3f)", mod1$se), sprintf("(%.3f)",mod2$se))
-
-# Fixed Effects
-parish_fe <- c("Parish FE", "Yes", "Yes")
-year_fe <- c("Year FE", "Yes", "Yes")
-
-# nObs
-twfe_obs <- c("Obs.", mod1$nobs, mod2$nobs)
-did_obs <- c("Obs.", agg_mod1$DIDparams$n, agg_mod2$DIDparams$n)
-
-# CS DiD coefficient
-did_coef <- c("RailAccess", sprintf("%.3f",agg_mod1$overall.att), sprintf("%.3f",agg_mod2$overall.att))
-
-# Add significance stars to DID coefficients based on p-values
-did_coef[2:3] <- sapply(2:3, function(i) {
-  coef_value <- as.numeric(did_coef[i])
-  p_value <- as.numeric(did_p[i-1])
-  if (!is.na(p_value)) {
-    if (p_value < 0.01) {
-      sprintf("%.3f***", coef_value)
-    } else if (p_value < 0.05) {
-      sprintf("%.3f**", coef_value)
-    } else if (p_value < 0.1) {
-      sprintf("%.3f*", coef_value)
-    } else {
-      sprintf("%.3f", coef_value)
-    }
-  } else {
-    sprintf("%.3f", coef_value)
-  }
-})
-
-
-did_se <- c("", sprintf("(%.3f)", agg_mod1$overall.se), sprintf("(%.3f)", agg_mod2$overall.se))
-
-
-# Create mean Outcome
-mean_outcome <- c(
-  "Mean of Outcome",
-  sapply(outcomes_cs, function(col) sprintf("%.3f", mean(census[[col]], na.rm = TRUE)))
-)
-
-# #Parishes
-did_n_parishes <- c("Parishes", length(unique(agg_mod1$DIDparams$data$GIS_ID)), length(unique(agg_mod2$DIDparams$data$GIS_ID)))
-
-# bind together for output table
-
-# bind together
-results <- rbind(twfe_coef, twfe_se, parish_fe, year_fe, twfe_obs, did_coef, did_se, did_n_parishes, did_obs, mean_outcome)
-colnames(results) <- c(" ", "(1)", "(2)")
-
-# Create Output latex table
-kbl(results, 
-    booktabs = T, 
-    caption = "Railways and Grundtvigianism", 
-    row.names = F,
-    align = "lcc",
-    linesep = "",  # Suppress additional line spacing
-    format = "latex") %>%
-  kable_styling(
-    latex_options = c("hold_position", "scale_down") # Ensures [!h] placement
-  ) %>%
-  add_header_above(outcomes_header) %>%
-  add_header_above(c(" " = 1, "Dependent variable:" = 2), escape = F) %>%
-  group_rows("Panel A: TWFE", 1, 5) %>%
-  group_rows("Panel B: Callaway and Sant'Anna", 6, 9) %>% 
-  footnote(
-    general = "Notes: This table...",
-    number = c("Clustered (Parish) Standard errors in parentheses."),
-    symbol = c("Significance levels: * p<0.10, ** p<0.05, *** p<0.01"),
-    threeparttable = TRUE, # Ensures LaTeX handles the footnotes correctly
-    escape = FALSE          # Allows LaTeX symbols in the notes
-  )
 
 
 # With Instrument ##########################################################################
@@ -975,8 +508,7 @@ tsls2 = feols(
 # first stages
 etable(tsls1$iv_first_stage[[1]], tsls2$iv_first_stage[[1]],
        tex = TRUE,
-       title = "First-Stage Estimates (With Nodes)")
-
+       title = "First-Stage Estimates (Nodes dropped)")
 
 twfe1_red <- feols(
   Assembly_house ~ LCPAccess 
@@ -1159,13 +691,13 @@ mean_outcome <- c(
 
 n_parishes <- c("Parishes", length(unique(agg_mod1$DIDparams$data$GIS_ID)), length(unique(agg_mod2$DIDparams$data$GIS_ID)))
 
-nodes_dropped <- c("Nodes dropped (< 10 km)", "No", "No")
 # first stage f-statistic
 firstF <- c("F-test (1st stage)", 
             round(as.numeric(fitstat(tsls1, "ivf")$ivf[1]), 2),
             round(as.numeric(fitstat(tsls2, "ivf")$ivf[1]), 2)
 )
 
+nodes_dropped <- c("Nodes dropped (< 10 km)", "Yes", "Yes")
 
 
 # bind together for output table
